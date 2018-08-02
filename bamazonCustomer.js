7d4e42406b1fb89dc137c566bf5ccf0c8a1462f9
j-bar-8 <josh.barkat@gmail.com>
2018-08-02 00:12:48 -0400

    getOrder function is retreiving and validating response

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -23,8 +23,26 @@ function displayAllProducts () {
             console.log(res[i].id + " || " + res[i].product_name + " || " +  res[i].price + " || " + res[i].stock_quantity);
         }
     })
+    getOrder();
 }
 
+function getOrder () {
+    inquirer
+    .prompt({
+        name: "id",
+        type: "input",
+        message: "What is the id # of the item you would like to purchase?",
+        validate: function(value) {
+            if (isNaN(value)=== false && value > 0) {
+                return true;
+            }
+            return false;
+        }
+    })
+    .then(function(answer) {
+        console.log("Buy item: " + answer.id);
+    })
+}
 // PROCESS
 // =====================================================================
 connection.connect(function(err) {
