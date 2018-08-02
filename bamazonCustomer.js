782135a000bcb4307693b37b76166530f811bb2b
j-bar-8 <josh.barkat@gmail.com>
2018-08-02 18:12:51 -0400

    added product object

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -9,6 +9,16 @@ var connection = mysql.createConnection({
     password: "root",
     database: "bamazon_DB"
 });
+var id = "";
+var quantityToBuy = 0;
+function Product(id, product, price, department, quantity) {
+    this.id = id;
+    this.product_name = product;
+    this.price = price;
+    this.department_name = department;
+    this.stock_quantity = quantity;
+}
+var newProduct = {};
 
 // FUNCTIONS
 // =====================================================================
@@ -28,20 +38,51 @@ function displayAllProducts () {
 
 function getOrder () {
     inquirer
-    .prompt({
+    .prompt([
+      {
         name: "id",
         type: "input",
-        message: "What is the id # of the item you would like to purchase?",
+        message: "What is the id# of the product you would like to purchase? ",
         validate: function(value) {
-            if (isNaN(value)=== false && value > 0) {
-                return true;
-            }
-            return false;
+          if (isNaN(value) === false && value > 0) {
+            return true;
+          }
+          return false;
         }
-    })
+      },
+      {
+        name: "quantity",
+        type: "input",
+        message: "How many would you like?",
+        validate: function(value) {
+          if (isNaN(value) === false && value > 0) {
+            return true;
+          }
+          return false;
+        }
+      }
+    ])
     .then(function(answer) {
-        console.log("Buy item: " + answer.id);
+        id = answer.id;
+        quantityToBuy = answer.quantity;
+        // console.log("Buy " + quantity + " of product #: " + product);
+        createProductObject();
     })
+}
+
+function createProductObject () {
+    connection.query("SELECT * FROM products WHERE id =?",[id], function(err, res) {
+        if (err) throw err;
+        // console.log(res[0].id);
+        newProduct = new Product (res[0].id, res[0].product_name, res[0].price, res[0].department_name, res[0].stock_quantity);
+        console.log(newProduct.id + newProduct.product_name + newProduct.price + newProduct.department_name + newProduct.stock_quantity);
+    })
+}
+
+function placeOrder () {
+    console.log ("Hit placeOrder" + "quantity: " + quantity + "product: " + id);
+    console.log (id - 1 );
+
 }
 // PROCESS
 // =====================================================================
